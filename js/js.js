4314eaeff175579d932496a82a160aa92edb0248
CEPJohnLocke <151115844+CEPJohnLocke@users.noreply.github.com>
2023-11-17 13:40:42 +0100

    Pedro

--- js/js.js
+++ js/js.js
@@ -1,6 +1,17 @@
 const menuMobileAdd = document.querySelector("#menu-mobile-add");
 const menuMobileRemove = document.querySelector("#menu-mobile-remove");
 const ul = document.querySelector("#ul");
+const btnSend = document.querySelector("#btnSend");
+const iName = document.querySelector("#iName");
+const iEmail = document.querySelector("#iEmail");
+const iAssunto = document.querySelector("#iAssunto");
+
+btnSend.addEventListener("click", (el)=> {
+    alert("A sua mensagem foi enviada com sucesso ✅");
+    iName.value = ""
+    iEmail.value = ""
+    iAssunto.value = ""
+})
 
 menuMobileAdd.addEventListener("click", (el) => {
     ul.classList.add("open");
